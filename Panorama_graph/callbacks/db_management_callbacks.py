8e36879ce4a4c54b948ba5fb2bcde32b7426a319
Fabien Graziani <fabien.graziani@inrae.fr>
2025-08-25 23:34:48 +0200

    Error correction for csv extraction.

--- Panorama_graph/callbacks/db_management_callbacks.py
+++ Panorama_graph/callbacks/db_management_callbacks.py
@@ -95,10 +95,10 @@ def on_click_load_gfa(n_clicks, gfa_file_names, chromosome_file, batch_size=DEFA
         else:
             list_chromosome_file = []
             for f in gfa_file_names:
-                if "_" in gfa_file_name:
-                    chromosome = gfa_file_name[:-4].split("_")[-1]
+                if "_" in f:
+                    chromosome = f[:-4].split("_")[-1]
                 else:
-                    chromosome = gfa_file_name[:-4]
+                    chromosome = f[:-4]
 
                 chromosome = chromosome.lower().removeprefix("chr")
                 chromosome = chromosome.lstrip("0")
