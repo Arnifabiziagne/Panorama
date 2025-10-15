e419edf56bc5f7faae65419cb698fb7f1fda183f
Fabien Graziani <fabien.graziani@inrae.fr>
2025-10-15 11:12:11 +0200

    Add gunicorn launcher.

---
+++ launch_gunicorn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+DASH_PORT=${1:-8050}
+# Stop on error
+set -e
+
+__conda_setup="$($(which conda) 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    echo "Erreur : conda not found."
+    exit 1
+fi
+
+conda activate panorama_graph
+
+echo "Launching Panorama on port $DASH_PORT..."
+
+
+python -m gunicorn wsgi:application -w 4 -b 0.0.0.0:$DASH_PORT
---
+++ wsgi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 15 10:46:24 2025
+
+@author: fgraziani
+"""
+
+from app import server
+import index  # pour initialiser l'app et callbacks
+
+application = server
